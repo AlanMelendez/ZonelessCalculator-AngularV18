29711073bc1a7d857bafb1a336c46487301a568e
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2024-10-29 11:17:14 -0600

    Add new function to practice tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -46,4 +46,57 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('router-outlet')).not.toBeNull();
 
   });
+
+
+
+  it('should render outlet-router wrapped with css classes', ()=>{
+
+ // Let's take the  div element, this 'container div' contain inside it the router element.
+    const divElement = compiled.querySelector('div');
+    // console.log(divElement);
+    // console.log(divElement.classList.value);
+
+    //Evaluate css class of div:
+    const cssClasses = 'min-w-screen min-h-screen bg-gray-100 flex items-center justify-center px-5 py-5'.split(' ')
+    const divClasses = divElement.classList.value.split(' ');
+
+    console.log('Clases to expect: ',cssClasses)
+
+    //Evaluate the div element not be null.
+    expect(divElement).not.toBeNull();
+
+    //Evaluate cssClasses with classes of divElement, verify be the same  class in each iteration.
+    divElement.classList.forEach((className:string)=>{
+      expect(cssClasses).toContain(className)
+    });
+
+    cssClasses.forEach((className:string)=>{
+        expect(divClasses).toContain(className)
+    });
+
+  });
+
+  it("shoul contain the 'buy me a beer' link", ()=>{
+
+    //tips to evalualte:
+    /**
+     * anchoElement.title = 'Buy me a beer'
+     * anchorElement.href = 'https://www.buymeacoffee.com/scottwindon'
+     */
+
+    //Let's take  "a" element.
+    const anchorElement = compiled.querySelector('a');
+    const titleToEvaluate:string = 'Buy me a beer'
+    const urlToEvaluate:string = 'https://www.buymeacoffee.com/scottwindon'
+
+    //Evaluate the element not be null
+    expect(anchorElement).not.toBeNull();
+
+    //Evaluate each value (titleToEvaluate, urlToEvaluate)
+    expect(anchorElement.href).toContain(urlToEvaluate);
+    expect(anchorElement.title).toContain(titleToEvaluate);
+
+
+  });
+
 });
