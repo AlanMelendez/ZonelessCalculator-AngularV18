152cb1d7972e492655a9e03be61c1d2c3565b9a4
Alan Cuevas Melendez <73464077+AlanMelendez@users.noreply.github.com>
2024-11-21 15:48:39 -0600

    Generate test to verify apply CSS class with signal behaviors and detectChanges

--- src/app/calculator/components/calculator-button/calculator-button.component.spec.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.spec.ts
@@ -17,13 +17,41 @@ describe('CalculatorButtonComponent', () => {
     compiled = fixture.nativeElement as HTMLElement;
     component = fixture.componentInstance;
 
+    fixture.detectChanges(); //Step to apply styles detect to first test. Line 28.
+
   });
 
   it('should create the app', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should apply w-1/4 doubleSizer is false', ()=>{
 
+    /* 
+        * Like this need apply this class after render component, it's necessary apply "detectChanges() at component in line 20"
+        * It's because use signals to capture styles and need detect changes.
+
+    */
+
+    const hostCssClasses: string[] = compiled.classList.value.split(' ')
+
+    expect(hostCssClasses).toContain('w-1/4')
+    expect(component.isDoubleSize()).toBe(false); //or also toBeFalse()
+    
+  })
+
+  it('should apply w-2/4 doubleSizer is true', ()=>{
+    
+    //Simulate button "isDoubleSize"
+    fixture.componentRef.setInput('isDoubleSize',true);
+    fixture.detectChanges();
+
+    const hostCssClasses: string[] = compiled.classList.value.split(' ');
+
+    expect(hostCssClasses).toContain('w-2/4');
+    expect(component.isDoubleSize()).toBeTrue();
+    
+  })
 
 
 });
