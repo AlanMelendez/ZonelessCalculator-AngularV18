48242f13874d5d0cb8e2075c02ab8d812b6c5f80
Alan Cuevas Melendez <73464077+AlanMelendez@users.noreply.github.com>
2024-11-21 16:29:20 -0600

    add new function tests using spy, done to evaluate content in functions with singnals

--- src/app/calculator/components/calculator-button/calculator-button.component.spec.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.spec.ts
@@ -38,7 +38,7 @@ describe('CalculatorButtonComponent', () => {
     expect(hostCssClasses).toContain('w-1/4')
     expect(component.isDoubleSize()).toBe(false); //or also toBeFalse()
     
-  })
+  });
 
   it('should apply w-2/4 doubleSizer is true', ()=>{
     
@@ -51,7 +51,41 @@ describe('CalculatorButtonComponent', () => {
     expect(hostCssClasses).toContain('w-2/4');
     expect(component.isDoubleSize()).toBeTrue();
     
-  })
+  });
+
+
+  it('should emit onClick  when handleClick function is called', ()=>{
+
+    //We use spy, the spy is like a mock, but it's a function that we can use to check if it was called, how many times, and with what parameters.
+    const spy = spyOn(component.onClick,'emit');
+
+    component.handleClick();
+
+    expect(component.onClick.emit).toHaveBeenCalled();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+
+  it('should set isPressed to true when keyPressStyle function is called with a matching key', (done)=>{
+
+    console.log(compiled); //to see the component structure and check value to content
+    //simulate keyPressStyle some key.
+    component.contentValue().nativeElement.innerText = '1';
+
+    console.log(compiled); //to see again the componente value if is changed to '1'
+
+
+    component.keyPressStyle('1');
+
+    expect(component.isPressed()).toBeTrue();
+
+    setTimeout(() => {
+      expect(component.isPressed()).toBeTrue();
+      done();
+    }, 200);
+
+  });
 
 
 });
