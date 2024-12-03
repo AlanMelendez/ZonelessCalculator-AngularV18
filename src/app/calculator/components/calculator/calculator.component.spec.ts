582b5f010750b51381467f78eabe2d0829004c95
Alan Cuevas Melendez <73464077+AlanMelendez@users.noreply.github.com>
2024-12-03 13:44:30 -0600

    All testing finished, exec npm run test:coverage to evaluate all of

--- src/app/calculator/components/calculator/calculator.component.spec.ts
+++ src/app/calculator/components/calculator/calculator.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CalculatorComponent } from './calculator.component';
 import { CalculatorService } from '@/calculator/services/calculator.service';
+import { CalculatorButtonComponent } from '../calculator-button/calculator-button.component';
+import { By } from '@angular/platform-browser';
 
 class calculatorMockService {
   public resultText = jasmine.createSpy('resultText').and.returnValue('100.00');
@@ -64,6 +66,59 @@ describe('CalculatorComponent', () => {
     // console.log('component', expect(compiled.querySelector('span')));
     expect(compiled.querySelector('span')?.textContent).toBe('100 -');
 
+
+  });
+
+
+  it('should have 19 calculator-button components', () => {
+        // Way to get the content projection using viewChildren to get component #1
+
+    expect(component.calculatorButtons()).toBeTruthy();
+    expect(component.calculatorButtons().length).toBe(19);
+  });
+
+  it('should have 19 calculator-button with content projection', () => {
+    // Way to get the content projection  #2
+    const button = compiled.querySelectorAll('calculator-button');
+    expect(button.length).toBe(19);
+
+    // Way to get the content projection #3
+
+    // const buttonsByDirective = fixture.debugElement.queryAll(
+    //   By.directive(CalculatorButtonComponent)
+    // );
+
+  });
+
+  it('should be the first button with the text "C"', () => {
+    const button = compiled.querySelector('calculator-button');
+    expect(button?.textContent?.trim()).toBe('C');
+  });
+
+  it('should handle keyboard events', () => {
+    const enterEvent = new KeyboardEvent('keyup', { key: 'Enter' });
+    // component.handleKeyboardEvent(event);
+    document.dispatchEvent(enterEvent);
+    expect(calculationService.constructNumber).toHaveBeenCalledWith('=');
+
+
+    const escapeEvent = new KeyboardEvent('keyup', { key: 'Escape' });
+    component.handleKeyboardEvent(escapeEvent);
+    expect(calculationService.constructNumber).toHaveBeenCalledWith('C');
+  });
+
+
+  it('should display the proper key press style', () => {
+    calculationService.resultText.and.returnValue('500');
+    calculationService.subResultText.and.returnValue('100');
+    calculationService.lastOperator.and.returnValue('-');
+    fixture.detectChanges();
+
+    // expect(compiled.querySelector('span')?.textContent).toBe('500');
+    expect(component.resultText()).toBe('500');
+
+    // console.log('component span value', compiled.querySelector('span')?.textContent);
+    expect(compiled.querySelector('span')?.textContent).toContain('100 -');
 
   });
 });
